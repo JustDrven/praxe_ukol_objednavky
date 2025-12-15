18ae148d2582adcae37ab12ff903a8c15c52f404
JustDrven <petr.nemec11111@gmail.com>
2025-12-15 13:06:33 +0100

    feat(program): create utilities

---
+++ Utilities/UtilList.cs
@@ -0,0 +1,48 @@
+﻿/*******************************************
+ * 
+ * Copyright (c) High School Blek, 2025
+ * 
+ * File:        UtilList.cs
+ * Author:      JustDrven
+ * 
+ *******************************************/
+
+namespace praxe_desktop_client.Utilities
+{
+    internal class UtilList
+    {
+
+        public static void Clear(ListBox list)
+        {
+            if (list == null) return;
+
+            var items = list.Items;
+            if (items.Count == 0) return;
+
+            items.Clear();
+
+        }
+
+
+        public static void ClearMap<TKey, TData>(Dictionary<TKey, TData>? map)
+        {
+            if (map == null) return;
+            if (map.Count == 0) return;
+
+            map.Clear();
+
+        }
+
+        public static void SetContent(ListBox list, Action a)
+        {
+            if (list == null) return;
+            Clear(list);
+
+
+
+            a.Invoke();
+
+        }
+
+    }
+}
---
+++ Utilities/UtilWindow.cs
@@ -0,0 +1,42 @@
+﻿/*******************************************
+ * 
+ * Copyright (c) High School Blek, 2025
+ * 
+ * File:        UtilWindow.cs
+ * Author:      JustDrven
+ * 
+ *******************************************/
+
+namespace praxe_desktop_client.Utilities
+{
+    internal class UtilWindow
+    {
+
+        public static bool IsWindowValid(Form f)
+        {
+            return (f != null) && (!f.IsDisposed);
+        }
+    
+        public static void DisableResizeable(Form f)
+        {
+            if (!IsWindowValid(f)) return;
+
+
+            f.MinimumSize = new Size(f.Width, f.Height);
+            f.MaximumSize = f.MinimumSize;
+
+        }
+
+        public static void SetTitle(Form f, string title)
+        {
+            if (!IsWindowValid(f)) return;
+
+
+            if (string.IsNullOrEmpty(title)) return;
+
+            f.Text = "Praxe - " + title;
+
+        }
+    
+    }
+}
